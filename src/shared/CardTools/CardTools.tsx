e45df014a2586f8e8447c02f8d7dc896336e56ce
Dmitry <dmitrygreshnov2@gmail.com>
2025-10-18 20:09:10 +0300

    fixed CardTools

--- src/shared/CardTools/CardTools.tsx
+++ src/shared/CardTools/CardTools.tsx
@@ -1,13 +1,7 @@
 import { ListCategory } from "./ListCategory/ListCategory"
 import styles from "./CardTools.module.css"
 
-type ToolType = "production" | "construction" | "car_service";
-
-type Props = {
-    type: ToolType;
-};
-
-export const CardTools: React.FC<Props> = ({item}) => {
+export const CardTools = ({item}: any) => {
     return (
         <div style={{ marginBottom: '40px' }}>
             <div className={styles.card_tools } id="container">
